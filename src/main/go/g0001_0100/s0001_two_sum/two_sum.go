ddd256d82fea91d7f1e89922b17ad007159b1819
Valentyn Kolesnikov <javadev75@gmail.com>
2024-01-27 03:39:55 +0200

    Improved task 1

---
+++ src/main/go/g0001_0100/s0001_two_sum/two_sum.go
@@ -0,0 +1,13 @@
+package s0001_two_sum
+
+func twoSum(nums []int, target int) []int {
+	lookup := make(map[int]int)
+	for i, v := range nums {
+		j, ok := lookup[v]
+		lookup[target-v] = i
+		if ok {
+			return []int{j, i}
+		}
+	}
+	return nil
+}
