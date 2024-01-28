fe437869fb76f8446f354f54531578ca0a13e3b2
Valentyn Kolesnikov <javadev75@gmail.com>
2024-01-28 19:33:46 +0200

    Fixed format

--- src/main/go/g0001_0100/s0001_two_sum/two_sum.go
+++ src/main/go/g0001_0100/s0001_two_sum/two_sum.go
@@ -1,13 +1,13 @@
 package s0001_two_sum
 
 func twoSum(numbers []int, target int) []int {
-    indexMap := make(map[int]int)
-    for i, num := range numbers {
-        requiredNum := target - num
-        if j, ok := indexMap[requiredNum]; ok {
-            return []int{j, i}
-        }
-        indexMap[num] = i
-    }
-    return []int{-1, -1}
+	indexMap := make(map[int]int)
+	for i, num := range numbers {
+		requiredNum := target - num
+		if j, ok := indexMap[requiredNum]; ok {
+			return []int{j, i}
+		}
+		indexMap[num] = i
+	}
+	return []int{-1, -1}
 }
--- src/main/go/g0001_0100/s0001_two_sum/two_sum_test.go
+++ src/main/go/g0001_0100/s0001_two_sum/two_sum_test.go
@@ -1,18 +1,18 @@
 package s0001_two_sum
 
 import (
-    "github.com/stretchr/testify/assert"
-    "testing"
+	"github.com/stretchr/testify/assert"
+	"testing"
 )
 
 func TestTwoSum(t *testing.T) {
-    assert.Equal(t, []int{0, 1}, twoSum([]int{2, 7, 11, 15}, 9))
+	assert.Equal(t, []int{0, 1}, twoSum([]int{2, 7, 11, 15}, 9))
 }
 
 func TestTwoSum2(t *testing.T) {
-    assert.Equal(t, []int{0, 2}, twoSum([]int{7, 11, 15}, 22))
+	assert.Equal(t, []int{0, 2}, twoSum([]int{7, 11, 15}, 22))
 }
 
 func TestTwoSum3(t *testing.T) {
-    assert.Equal(t, []int{0, 1}, twoSum([]int{11, 15}, 26))
+	assert.Equal(t, []int{0, 1}, twoSum([]int{11, 15}, 26))
 }
