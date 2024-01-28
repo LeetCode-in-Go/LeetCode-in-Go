bceb9096c93c0b02a3a49b787f734d5be3a5be3c
Valentyn Kolesnikov <javadev75@gmail.com>
2024-01-29 01:31:48 +0200

    Improved task 1

--- src/main/go/g0001_0100/s0001_two_sum/solution_test.go
+++ src/main/go/g0001_0100/s0001_two_sum/solution_test.go
@@ -5,18 +5,18 @@ import (
 	"testing"
 )
 
-func TestTwoSum(t *testing.T) {
+func TwoSum(t *testing.T) {
 	assert.Equal(t, []int{0, 1}, twoSum([]int{2, 7, 11, 15}, 9))
 }
 
-func TestTwoSum2(t *testing.T) {
+func TwoSum2(t *testing.T) {
 	assert.Equal(t, []int{1, 2}, twoSum([]int{3, 2, 4}, 6))
 }
 
-func TestTwoSum3(t *testing.T) {
+func TwoSum3(t *testing.T) {
 	assert.Equal(t, []int{0, 1}, twoSum([]int{3, 3}, 6))
 }
 
-func TestTwoSum4(t *testing.T) {
+func TwoSum4(t *testing.T) {
 	assert.Equal(t, []int{-1, -1}, twoSum([]int{3, 3}, 7))
 }
