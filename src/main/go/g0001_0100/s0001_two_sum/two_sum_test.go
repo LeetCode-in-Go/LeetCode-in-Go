fbab3ccec24c2f2c19f55b2f502e003f6aa93efc
Valentyn Kolesnikov <javadev75@gmail.com>
2024-01-28 14:15:59 +0200

    Improved test

--- src/main/go/g0001_0100/s0001_two_sum/two_sum_test.go
+++ src/main/go/g0001_0100/s0001_two_sum/two_sum_test.go
@@ -1,27 +1,19 @@
 package s0001_two_sum
 
 import (
+	"github.com/stretchr/testify/assert"
 	"reflect"
 	"testing"
 )
 
-var (
-	case1, target1, result1 = []int{2, 7, 11, 15}, 9, []int{0, 1}
-	case2, target2, result2 = []int{7, 11, 15}, 22, []int{0, 2}
-	case3, target3, result3 = []int{11, 15}, 26, []int{0, 1}
-)
-
 func TestTwoSum(t *testing.T) {
-	if !reflect.DeepEqual(twoSum(case1, target1), result1) {
-		t.Fail()
-	}
-	if !reflect.DeepEqual(twoSum(case2, target2), result2) {
-		t.Fail()
-	}
-	if !reflect.DeepEqual(twoSum(case3, target3), result3) {
-		t.Fail()
-	}
-	if twoSum(nil, 0) != nil {
-		t.Fail()
-	}
+	assert.Equal(t, []int{0, 1}, twoSum([]int{2, 7, 11, 15}, 9))
+}
+
+func TestTwoSum2(t *testing.T) {
+	assert.Equal(t, []int{0, 2}, twoSum([]int{7, 11, 15}, 22))
+}
+
+func TestTwoSum3(t *testing.T) {
+	assert.Equal(t, []int{0, 1}, twoSum([]int{11, 15}, 26))
 }
