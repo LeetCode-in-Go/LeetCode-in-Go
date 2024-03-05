be830d53b6199d51d3980a0eeba6254df51bd6fd
Thanh829 <93962044+ThanhNIT@users.noreply.github.com>
2024-03-05 14:47:29 +0700

    Added tasks 2-6

---
+++ src/main/go/g0001_0100/s0005_longest_palindromic_substring/solution_test.go
@@ -0,0 +1,15 @@
+package s0005_longest_palindromic_substring
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestLongestPalindrome(t *testing.T) {
+	assert.Equal(t, "bab", longestPalindrome("babad"))
+}
+
+func TestLongestPalindrome2(t *testing.T) {
+	assert.Equal(t, "bb", longestPalindrome("cbbd"))
+}
---
+++ src/main/go/g0001_0100/s0006_zigzag_conversion/solution.go
@@ -0,0 +1,35 @@
+package s0006_zigzag_conversion
+
+// #Medium #String #2024_03_05_Time_0_ms_(100.00%)_Space_4_MB_(86.83%)
+
+import "strings"
+
+func convert(s string, numRows int) string {
+	sLen := len(s)
+	if numRows == 1 {
+		return s
+	}
+	maxDist := numRows*2 - 2
+	var buf strings.Builder
+
+	for i := 0; i < numRows; i++ {
+		index := i
+		if i == 0 || i == numRows-1 {
+			for index < sLen {
+				buf.WriteByte(s[index])
+				index += maxDist
+			}
+		} else {
+			for index < sLen {
+				buf.WriteByte(s[index])
+				index += maxDist - i*2
+				if index >= sLen {
+					break
+				}
+				buf.WriteByte(s[index])
+				index += i * 2
+			}
+		}
+	}
+	return buf.String()
+}
