33809c54e71e46f2806b41fbe84b70d5e646beff
Thanh829 <93962044+ThanhNIT@users.noreply.github.com>
2024-03-07 10:49:01 +0700

    Update solution.go

--- src/main/go/g0001_0100/s0010_regular_expression_matching/solution.go
+++ src/main/go/g0001_0100/s0010_regular_expression_matching/solution.go
@@ -5,35 +5,30 @@ package s0010_regular_expression_matching
 // #2024_03_07_Time_0_ms_(100.00%)_Space_2.3_MB_(36.02%)
 
 func isMatch(s string, p string) bool {
-	textLength := len(s)
-	patternLength := len(p)
-	result := make([][]bool, textLength+1)
-	for i := range result {
-		result[i] = make([]bool, patternLength+1)
+	m := len(s)
+	n := len(p)
+	dp := make([][]bool, m+1)
+	for i := 0; i <= m; i++ {
+		dp[i] = make([]bool, n+1)
 	}
-	result[0][0] = true
-	for j := 2; j < patternLength+1; {
-		if p[j-1] == '*' {
-			result[0][j] = result[0][j-2]
+	dp[0][0] = true
+	for i := 1; i <= n; i++ {
+		if p[i-1] == '*' {
+			dp[0][i] = dp[0][i-2]
 		}
-		j++
 	}
-	for i := 0; i < textLength; {
-		for j := 0; j < patternLength; {
-			if p[j] == s[i] || p[j] == '.' {
-				result[i+1][j+1] = result[i][j]
-			} else if p[j] == '*' {
-				if p[j-1] == s[i] || p[j-1] == '.' {
-					result[i+1][j+1] = result[i+1][j-1] || result[i][j+1]
-				} else {
-					result[i+1][j+1] = result[i+1][j-1]
+	for i := 1; i <= m; i++ {
+		for j := 1; j <= n; j++ {
+			if p[j-1] == '.' || p[j-1] == s[i-1] {
+				dp[i][j] = dp[i-1][j-1]
+			} else if p[j-1] == '*' {
+				dp[i][j] = dp[i][j-2]
+				if s[i-1] == p[j-2] || p[j-2] == '.' {
+					dp[i][j] = dp[i][j] || dp[i-1][j]
 				}
-			} else {
-				result[i+1][j+1] = false
+
 			}
-			j++
 		}
-		i++
 	}
-	return result[textLength][patternLength]
+	return dp[m][n]
 }
