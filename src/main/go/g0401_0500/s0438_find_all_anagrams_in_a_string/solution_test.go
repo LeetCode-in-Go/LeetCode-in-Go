c849da1dbdfa957fa7041695ecdc30cf85372b26
Valentyn Kolesnikov <javadev75@gmail.com>
2024-03-14 08:05:00 +0200

    Improved task 438

--- src/main/go/g0401_0500/s0438_find_all_anagrams_in_a_string/solution_test.go
+++ src/main/go/g0401_0500/s0438_find_all_anagrams_in_a_string/solution_test.go
@@ -2,20 +2,25 @@ package s0438_find_all_anagrams_in_a_string
 
 import (
 	"reflect"
+	"sort"
 	"testing"
 )
 
 func TestFindAnagrams(t *testing.T) {
-	result := findAnagrams("cbaebabacd", "abc")
+	result := Solution{}.findAnagrams("cbaebabacd", "abc")
 	expected := []int{0, 6}
+	sort.Ints(result)
+	sort.Ints(expected)
 	if !reflect.DeepEqual(result, expected) {
 		t.Errorf("Expected: %v, but got: %v", expected, result)
 	}
 }
 
 func TestFindAnagrams2(t *testing.T) {
-	result := findAnagrams("abab", "ab")
+	result := Solution{}.findAnagrams("abab", "ab")
 	expected := []int{0, 1, 2}
+	sort.Ints(result)
+	sort.Ints(expected)
 	if !reflect.DeepEqual(result, expected) {
 		t.Errorf("Expected: %v, but got: %v", expected, result)
 	}
