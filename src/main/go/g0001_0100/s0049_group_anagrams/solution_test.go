be621e1f9f2a35f68f729fe732be72fb4a56e74a
Valentyn Kolesnikov <javadev75@gmail.com>
2024-03-15 06:06:26 +0200

    Improved test 49

--- src/main/go/g0001_0100/s0049_group_anagrams/solution_test.go
+++ src/main/go/g0001_0100/s0049_group_anagrams/solution_test.go
@@ -7,7 +7,7 @@ import (
 )
 
 func TestGroupAnagrams(t *testing.T) {
-	expected := [][]string{{"eat", "tea", "ate"}, {"bat"}, {"tan", "nat"}}
+	expected := [][]string{{"bat"}, {"nat", "tan"}, {"ate", "eat", "tea"}}
 	actual := groupAnagrams([]string{"eat", "tea", "tan", "ate", "nat", "bat"})
 	sort.Slice(actual, func(i, j int) bool {
 		return len(actual[i]) < len(actual[j])
@@ -23,12 +23,6 @@ func TestGroupAnagrams(t *testing.T) {
 func TestGroupAnagrams2(t *testing.T) {
 	expected := [][]string{{""}}
 	actual := groupAnagrams([]string{""})
-	sort.Slice(actual, func(i, j int) bool {
-		return len(actual[i]) < len(actual[j])
-	})
-	for i := range actual {
-		sort.Strings(actual[i])
-	}
 	if !reflect.DeepEqual(actual, expected) {
 		t.Errorf("Expected: %v, but got: %v", expected, actual)
 	}
@@ -37,12 +31,6 @@ func TestGroupAnagrams2(t *testing.T) {
 func TestGroupAnagrams3(t *testing.T) {
 	expected := [][]string{{"a"}}
 	actual := groupAnagrams([]string{"a"})
-	sort.Slice(actual, func(i, j int) bool {
-		return len(actual[i]) < len(actual[j])
-	})
-	for i := range actual {
-		sort.Strings(actual[i])
-	}
 	if !reflect.DeepEqual(actual, expected) {
 		t.Errorf("Expected: %v, but got: %v", expected, actual)
 	}
