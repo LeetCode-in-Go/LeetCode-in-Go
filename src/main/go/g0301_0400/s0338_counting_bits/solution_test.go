441a3295f5a10be1c2cd2c640445b4d15c50ca0e
Valentyn Kolesnikov <javadev75@gmail.com>
2024-03-16 00:52:13 -0400

    Added tasks 300-394

---
+++ src/main/go/g0301_0400/s0338_counting_bits/solution_test.go
@@ -0,0 +1,18 @@
+package s0338_counting_bits
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestCountBits(t *testing.T) {
+	result := countBits(2)
+	expected := []int{0, 1, 1}
+	assert.Equal(t, expected, result)
+}
+
+func TestCountBits2(t *testing.T) {
+	result := countBits(5)
+	expected := []int{0, 1, 1, 2, 1, 2}
+	assert.Equal(t, expected, result)
+}
