dd56586b3c03c18ae4d04039385e5310e0c45ae7
Valentyn Kolesnikov <javadev75@gmail.com>
2024-03-16 06:57:44 +0200

    Improved tasks 49, 416

--- src/main/go/g0001_0100/s0049_group_anagrams/solution_test.go
+++ src/main/go/g0001_0100/s0049_group_anagrams/solution_test.go
@@ -1,37 +1,25 @@
 package s0049_group_anagrams
 
 import (
+	"github.com/stretchr/testify/assert"
 	"reflect"
-	"sort"
 	"testing"
 )
 
 func TestGroupAnagrams(t *testing.T) {
 	expected := [][]string{{"bat"}, {"nat", "tan"}, {"ate", "eat", "tea"}}
 	actual := groupAnagrams([]string{"eat", "tea", "tan", "ate", "nat", "bat"})
-	sort.Slice(actual, func(i, j int) bool {
-		return len(actual[i]) < len(actual[j])
-	})
-	for i := range actual {
-		sort.Strings(actual[i])
-	}
-	if !reflect.DeepEqual(actual, expected) {
-		t.Errorf("Expected: %v, but got: %v", expected, actual)
-	}
+	assert.True(t, reflect.DeepEqual(expected, actual))
 }
 
 func TestGroupAnagrams2(t *testing.T) {
 	expected := [][]string{{""}}
 	actual := groupAnagrams([]string{""})
-	if !reflect.DeepEqual(actual, expected) {
-		t.Errorf("Expected: %v, but got: %v", expected, actual)
-	}
+	assert.True(t, reflect.DeepEqual(expected, actual))
 }
 
 func TestGroupAnagrams3(t *testing.T) {
 	expected := [][]string{{"a"}}
 	actual := groupAnagrams([]string{"a"})
-	if !reflect.DeepEqual(actual, expected) {
-		t.Errorf("Expected: %v, but got: %v", expected, actual)
-	}
+	assert.True(t, reflect.DeepEqual(expected, actual))
 }
