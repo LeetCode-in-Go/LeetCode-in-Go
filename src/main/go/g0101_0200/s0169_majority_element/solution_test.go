23fe017ed56177fabef10c82395ce6b2469f4468
Thanh829 <93962044+ThanhNIT@users.noreply.github.com>
2024-03-22 10:37:52 +0700

    Update solution_test.go

--- src/main/go/g0101_0200/s0169_majority_element/solution_test.go
+++ src/main/go/g0101_0200/s0169_majority_element/solution_test.go
@@ -5,10 +5,10 @@ import (
 	"testing"
 )
 
-func Test_majorityElement(t *testing.T) {
+func TestMajorityElement(t *testing.T) {
 	assert.Equal(t, 3, majorityElement([]int{3, 2, 3}))
 }
 
-func Test_majorityElement2(t *testing.T) {
+func TestMajorityElement2(t *testing.T) {
 	assert.Equal(t, 2, majorityElement([]int{2, 2, 1, 1, 1, 2, 2}))
 }
