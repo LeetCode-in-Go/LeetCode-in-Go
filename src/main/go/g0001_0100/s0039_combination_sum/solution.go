e3f9fda9c746dc8c6759e8ee20b3d5207bffeb0e
Valentyn Kolesnikov <javadev75@gmail.com>
2025-04-27 06:19:08 +0300

    Updated tasks 1-39

--- src/main/go/g0001_0100/s0039_combination_sum/solution.go
+++ src/main/go/g0001_0100/s0039_combination_sum/solution.go
@@ -3,7 +3,7 @@ package s0039_combination_sum
 // #Medium #Top_100_Liked_Questions #Array #Backtracking #Algorithm_II_Day_10_Recursion_Backtracking
 // #Level_2_Day_20_Brute_Force/Backtracking #Udemy_Backtracking/Recursion
 // #Top_Interview_150_Backtracking #Big_O_Time_O(2^n)_Space_O(n+2^n)
-// #2024_03_11_Time_0_ms_(100.00%)_Space_3_MB_(71.24%)
+// #2025_04_27_Time_0_ms_(100.00%)_Space_4.97_MB_(71.50%)
 
 func combinationSum(coins []int, amount int) [][]int {
 	var ans [][]int
