0ee1e7c96fd0764b4598304c5dddb50ce4dce5a6
Valentyn Kolesnikov <javadev75@gmail.com>
2025-05-03 11:05:55 +0300

    Updated tags for tasks 41-48

--- src/main/go/g0001_0100/s0042_trapping_rain_water/solution.go
+++ src/main/go/g0001_0100/s0042_trapping_rain_water/solution.go
@@ -3,7 +3,7 @@ package s0042_trapping_rain_water
 // #Hard #Top_100_Liked_Questions #Top_Interview_Questions #Array #Dynamic_Programming #Two_Pointers
 // #Stack #Monotonic_Stack #Dynamic_Programming_I_Day_9 #Udemy_Two_Pointers
 // #Top_Interview_150_Array/String #Big_O_Time_O(n)_Space_O(1)
-// #2024_03_12_Time_3_ms_(99.42%)_Space_5.4_MB_(97.01%)
+// #2025_05_03_Time_0_ms_(100.00%)_Space_7.85_MB_(82.53%)
 
 func trap(height []int) int {
 	if height == nil {
