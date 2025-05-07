335376e3e04345471b9488a3c4e08f2fdeba9ecd
ThanhNIT <93962044+ThanhNIT@users.noreply.github.com>
2025-05-08 00:23:42 +0700

    Updated tags for tasks 114-142

--- src/main/go/g0101_0200/s0128_longest_consecutive_sequence/solution.go
+++ src/main/go/g0101_0200/s0128_longest_consecutive_sequence/solution.go
@@ -2,7 +2,7 @@ package s0128_longest_consecutive_sequence
 
 // #Medium #Top_100_Liked_Questions #Top_Interview_Questions #Array #Hash_Table #Union_Find
 // #Top_Interview_150_Hashmap #Big_O_Time_O(N_log_N)_Space_O(1)
-// #2024_03_19_Time_54_ms_(99.84%)_Space_8.9_MB_(93.24%)
+// #2025_05_07_Time_4_ms_(99.84%)_Space_10.05_MB_(99.24%)
 
 import "sort"
 
