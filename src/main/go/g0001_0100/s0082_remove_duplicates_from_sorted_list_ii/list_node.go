0ad37ed2d9edb1d8b913a2a8ca435705655276b4
Valentyn Kolesnikov <javadev75@gmail.com>
2025-05-14 20:57:00 +0300

    Improved tasks 61, 82

--- src/main/go/g0001_0100/s0082_remove_duplicates_from_sorted_list_ii/list_node.go
+++ src/main/go/g0001_0100/s0082_remove_duplicates_from_sorted_list_ii/list_node.go
@@ -1,4 +1,4 @@
-package list_node
+package s0082_remove_duplicates_from_sorted_list_ii
 
 type ListNode struct {
 	Val  int
--- src/main/go/g0001_0100/s0082_remove_duplicates_from_sorted_list_ii/solution_test.go
+++ src/main/go/g0001_0100/s0082_remove_duplicates_from_sorted_list_ii/solution_test.go
@@ -3,12 +3,11 @@ package s0082_remove_duplicates_from_sorted_list_ii
 import (
 	"testing"
 
-	"github.com/LeetCode-in-Go/LeetCode-in-Go/src/main/go/com_github_leetcode/list_node"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestDeleteDuplicates(t *testing.T) {
-	head := &list_node.ListNode{Val: 1, Next: &list_node.ListNode{Val: 2, Next: &list_node.ListNode{Val: 3, Next: &list_node.ListNode{Val: 3, Next: &list_node.ListNode{Val: 4, Next: &list_node.ListNode{Val: 4, Next: &list_node.ListNode{Val: 5}}}}}}}
+	head := &ListNode{Val: 1, Next: &ListNode{Val: 2, Next: &ListNode{Val: 3, Next: &ListNode{Val: 3, Next: &ListNode{Val: 4, Next: &ListNode{Val: 4, Next: &ListNode{Val: 5}}}}}}}
 	result := deleteDuplicates(head)
 	assert.Equal(t, 1, result.Val)
 	assert.Equal(t, 2, result.Next.Val)
@@ -16,7 +15,7 @@ func TestDeleteDuplicates(t *testing.T) {
 }
 
 func TestDeleteDuplicates2(t *testing.T) {
-	head := &list_node.ListNode{Val: 1, Next: &list_node.ListNode{Val: 1, Next: &list_node.ListNode{Val: 1, Next: &list_node.ListNode{Val: 2, Next: &list_node.ListNode{Val: 3}}}}}
+	head := &ListNode{Val: 1, Next: &ListNode{Val: 1, Next: &ListNode{Val: 1, Next: &ListNode{Val: 2, Next: &ListNode{Val: 3}}}}}
 	result := deleteDuplicates(head)
 	assert.Equal(t, 2, result.Val)
 	assert.Equal(t, 3, result.Next.Val)
