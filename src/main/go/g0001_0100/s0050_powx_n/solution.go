2a7121b0619dc5f59490046a4ac621945f5c2174
Valentyn Kolesnikov <javadev75@gmail.com>
2025-05-24 20:06:12 +0300

    Fixed format

--- src/main/go/g0001_0100/s0050_powx_n/solution.go
+++ src/main/go/g0001_0100/s0050_powx_n/solution.go
@@ -4,22 +4,22 @@ package s0050_powx_n
 // #2025_05_24_Time_0_ms_(100.00%)_Space_3.88_MB_(100.00%)
 
 func myPow(x float64, n int) float64 {
-    nn := int64(n)
-    res := 1.0
-    if n < 0 {
-        nn = -nn
-    }
-    for nn > 0 {
-        if nn%2 == 1 {
-            nn--
-            res *= x
-        } else {
-            x *= x
-            nn /= 2
-        }
-    }
-    if n < 0 {
-        return 1.0 / res
-    }
-    return res
+	nn := int64(n)
+	res := 1.0
+	if n < 0 {
+		nn = -nn
+	}
+	for nn > 0 {
+		if nn%2 == 1 {
+			nn--
+			res *= x
+		} else {
+			x *= x
+			nn /= 2
+		}
+	}
+	if n < 0 {
+		return 1.0 / res
+	}
+	return res
 }
